90d70ff029939cf4a7985f76bbeaa5f269eacb81
Miroslav Biňas <miroslav.binas@tuke.sk>
2021-03-30 21:46:36 +0200

    pridany vysledok webinara

--- 2021/03-neinovativna.metoda/resources/game.py
+++ 2021/03-neinovativna.metoda/resources/game.py
@@ -1,67 +1,56 @@
-#!/usr/bin/env pgzrun
-
-# window's attributes
-TITLE = 'Arkanoid.py'
-WIDTH = 640
 HEIGHT = 400
+WIDTH = 640
+TITLE = "Arkanoid"
 
-# ball's attributes
 ball = Actor('ball')
 ball.x = WIDTH / 2
 ball.y = HEIGHT / 2
+ball.speed = 5
 ball.dx = 1
 ball.dy = -1
-ball.speed = 3
 
-# paddle's attributes
 paddle = Actor('paddle')
 paddle.x = WIDTH / 2
 paddle.bottom = HEIGHT
-paddle.speed = 3
+paddle.speed = 4
 
-# creating bricks
 bricks = []
 for i in range(10):
     brick = Actor('brick.red')
     brick.left = brick.width * i
     bricks.append(brick)
 
-
 def update():
-    # update ball
     ball.x = ball.x + ball.dx * ball.speed
     ball.y = ball.y + ball.dy * ball.speed
-
-    if ball.top <= 0:
-        ball.dy = 1
 
     if ball.right >= WIDTH:
         ball.dx = -1
 
-    if ball.bottom >= HEIGHT:
-        #ball.dy = -1
-        print('You are Loser')
-        quit()
+    if ball.top <= 0:
+        ball.dy = +1
 
     if ball.left <= 0:
-        ball.dx = 1
+        ball.dx = +1
 
-    if ball.colliderect(paddle):
-        ball.dy = ball.dy * -1
+    if ball.bottom >= HEIGHT:
+        print('Game over')
+        quit()
+        #ball.dy = -1
 
-    # update brick
     for brick in bricks:
-        if ball.colliderect(brick) == True:
+        if ball.colliderect(brick):
             bricks.remove(brick)
-            ball.dy = ball.dy * -1
+            ball.dy *= -1
             break
 
-    # well done
     if len(bricks) == 0:
-        print('weel done')
+        print('Well done')
         quit()
 
-    # update paddle
+    # god mode
+    paddle.x = ball.x
+
     if keyboard.left == True:
         paddle.x = paddle.x - paddle.speed
         if paddle.left <= 0:
@@ -72,7 +61,9 @@ def update():
         if paddle.right >= WIDTH:
             paddle.right = WIDTH
 
-    paddle.x = ball.x
+    if paddle.colliderect(ball):
+        ball.dy *= -1
+        ball.bottom = paddle.top
 
 
 def draw():
@@ -82,5 +73,4 @@ def draw():
         brick.draw()
 
     ball.draw()
-
     paddle.draw()
