13884732e07b8fd267810671f542b9c1604b753a
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-11-16 09:11:15 +0100

    new talk

---
+++ 2022/11-uvptechnicom/resources/project/README.md
@@ -0,0 +1,3 @@
+# Mega Supa App
+
+Example app
---
+++ 2022/11-uvptechnicom/resources/project/supa/main.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+import uvicorn
+
+
+# fastapi app init
+app = FastAPI()
+app.mount("/static", StaticFiles(directory=Path(__file__).parent / 'static'), name="static")
+templates = Jinja2Templates(directory=Path(__file__).parent / 'templates')
+
+
+# homepage endpoint
+@app.get('/')
+def homepage(request: Request):
+    return templates.TemplateResponse("homepage.html", {'request': request})
+
+
+# main entrypoint
+def main():
+    uvicorn.run('supa.main:app', reload=True, host='0.0.0.0')
