6dd6ff43e765d336420c499d0d62e84cc92e611e
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-11-16 19:04:40 +0100

    refactoring

--- 2022/11-uvptechnicom/resources/project/supa/main.py
+++ 2022/11-uvptechnicom/resources/project/supa/main.py
@@ -6,7 +6,8 @@ from fastapi.staticfiles import StaticFiles
 import uvicorn
 
 import logging
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(name)-12s %(levelname)-8s: %(message)s')
+logger = logging.getLogger('supa')
 
 # fastapi app init
 app = FastAPI()
@@ -17,13 +18,32 @@ templates = Jinja2Templates(directory=Path(__file__).parent / 'templates')
 # homepage endpoint
 @app.get('/')
 def homepage(request: Request):
+    logger.info(f'Incomming client from {request.client.host}')
     return templates.TemplateResponse("homepage.html", {'request': request})
+
+
+@app.get('/deadlock')
+def deadlock():
+    logger.info('Ta tu si sa dostal')
+
+    while True:
+        pass
+
+    logger.info('ta ale odtialto sa uz nedostanes')
+
+
+@app.get('/health')
+def check_health():
+    return {
+        'status': 'up'
+    }
 
 
 # main entrypoint
 def main():
-    logging.info('Starting Application')
-    uvicorn.run('supa.main:app', reload=True, host='0.0.0.0', log_level="critical")
+    logger.info('Starting Application')
+    uvicorn.run('supa.main:app', reload=True, host='0.0.0.0')
+    #, log_level="critical")
 
 
 if __name__ == '__main__':
